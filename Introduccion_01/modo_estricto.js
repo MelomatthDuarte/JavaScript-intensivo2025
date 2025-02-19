10bbc8a4251257daa58b89269de1574789255639
melomatthewdev <devscustomers@outlook.com>
2025-02-19 09:59:49 -0500

    modo estricto

---
+++ Introduccion_01/modo_estricto.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Previene asignar valores a variables no declaradas anteriormente:
+variablesDeclaracion = 17;
+
+// Previene la reasignacion de valores globales:
+NaN = 10;
+
+// Previene parametros duplicados en funciones
+function suma(x, y) {
+    return x + y;
+}
+
