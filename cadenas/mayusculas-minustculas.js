39392f8dfe618570e7016bbef4b3eb271dd90800
melomatthewdev <devscustomers@outlook.com>
2025-03-02 12:04:45 -0500

    cadenas in Javascript

---
+++ cadenas/mayusculas-minustculas.js
@@ -0,0 +1,7 @@
+// La clase String contiene varios metodos que utilizamos para manipular strings:
+
+const textoEnMinuscula = `Lorem ipsum dolor sit amet consectetur adipisicing elit.`;
+const textoEnMayuscula = textoEnMinuscula.toUpperCase();
+console.log(textoEnMayuscula);
+console.log(textoEnMayuscula.toLowerCase());
+
